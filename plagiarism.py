f84dc81b17d8bc10f0e4ca0ad168760d3d75c8ce
dipanshagupta <dipanshagupta>
2018-03-14 10:02:07 -0400

    Calculate similarity

--- plagiarism.py
+++ plagiarism.py
@@ -2,7 +2,9 @@ import sys
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import os
+import Levenshtein
 import argparse
+from numpy import median
 
 # Function to clean the data i.e. fill empty fields, etc
 def cleanData(dataframe):
@@ -13,14 +15,46 @@ def cleanData(dataframe):
     # Convert everything to lower case
     dataframe = dataframe.apply(lambda x: x.apply(lambda x: str(x).lower()))
 
+def levDistance(str1, str2):
+    if str(str1) == "noanswer" or str(str2) == "noanswer": return 0
+    else: return Levenshtein.ratio(str(str1), str(str2))
+
 # Function to calculate metric i.e. detec plagiarism
 def calculateMetrics(dataframe):
     # Clean the data obtained
     cleanData(dataframe)
-    for i in dataframe.columns:
-        value_counts = dataframe[i].value_counts()
-        print value_counts
+    
+    # Obtain data as matrix, for easier traversal
+    data = dataframe.as_matrix()
+    
+    # This matrix will store similarity between ith and jth student
+    median_similarity = []
+    
+    # For every row i (or student i)
+    for i in range(len(data)):
+        medians_for_ith = []
+        
+        # For every row j (or student j)
+        for j in range(len(data)):
+            
+            # If same row, skip
+            if i == j:
+                medians_for_ith.append(0)
+                continue
+            else:
+                
+                list = []
+                # Find similarity ratio in every column i.e. ratio for all questions
+                for k in range(len(data[i])):
+                    ratio = levDistance(data[i][k], data[j][k])
+                    list.append(ratio)
+                
+                # Find median for this list of ratios, this will give us the median score between these 2 student
+                medians_for_ith.append(median(list))
 
+        median_similarity.append(medians_for_ith)
+
+print median_similarity
 
 # Main code
 
